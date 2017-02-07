b1548055112fee0c72beab3fca3a91f2db796958
ntujvang <ntuj.vang@holbertonschool.com>
2017-02-07 23:01:13 +0000

    working console with quit/EOF

--- console.py
+++ console.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import cmd, sys
+
+class hbnb(cmd.Cmd):
+    prompt = '(hbnb) '
+    file = None #maybe remove
+
+    def do_quit(self, arg):
+        'Quit command to exit the program'
+        return True
+
+    def do_EOF(self, arg):
+        'Exits the program'
+        return True
+
+if __name__ == '__main__':
+    hbnb().cmdloop()
