ce4b506c1a394354294eb97f7feec667f790cde0
ntujvang <ntuj.vang@holbertonschool.com>
2017-02-08 01:17:59 +0000

    added new arg for initializer

--- models/base_model.py
+++ models/base_model.py
@@ -4,20 +4,23 @@ import datetime, uuid, storage
 
 class BaseModel():
 
-    def __init__(self, id='', created_at='', updated_at=''):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.datetime.utcnow())
-        self.updated_at = str(datetime.datetime.utcnow())
+        self.created_at = datetime.datetime.utcnow()
+        self.updated_at = datetime.datetime.utcnow()
 
     def save(self):
-        self.updated_at = str(datetime.datetime.utcnow())
+        self.updated_at = datetime.datetime.utcnow()
 
     def to_json(self):
         my_dict = self.__dict__
+        for x in my_dict.keys():
+            if (isinstance(my_dict[x], datetime)):
+                my_dict[x] = str(my_dict[x])
         my_dict['__class__'] = type(self).__name__
         return my_dict
 
     def __str__(self):
         class_name = type(self).__name__
         id_string = str(self.id)
-        return "[" + class_name + "] (" + id_string + str(self.__dict__) + ")"
+        return ("[{}] ({}) {}".format(class_name, id_string, self.__dict__))
