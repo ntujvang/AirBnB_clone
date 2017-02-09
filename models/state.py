ae002d237b6200f651202f52a581cf1102b405e4
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-09 22:41:51 +0000

    edit comments

--- models/state.py
+++ models/state.py
@@ -8,10 +8,10 @@ state contains the 'State' class, a sub-class of BaseModel.
 
 class State(BaseModel):
     '''This is the 'State' class.
+
+    State contains one public attribute, 'name'.
     '''
     def __init__(self):
         '''This is the initialization function.
-
-        This function contains one attribute, 'name'.
         '''
         self.name = ''
