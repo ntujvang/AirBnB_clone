653a40c6aafdf065a49b18334a0c5af9088ba32a
ntujvang <ntuj.vang@holbertonschool.com>
2017-02-09 02:39:03 +0000

    100% create and 50% working show

--- console.py
+++ console.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import cmd, sys
+import cmd, sys, models
 
 class hbnb(cmd.Cmd):
     prompt = '(hbnb) '
+    obj = models.storage.all()
     file = None #maybe remove
 
     def do_quit(self, arg):
@@ -12,6 +13,30 @@ class hbnb(cmd.Cmd):
     def do_EOF(self, arg):
         'Exits the program'
         return True
+
+    def emptyline(self):
+        'Prevents repeat of previous input'
+        pass
+
+    def do_create(self, arg):
+        'Creates a new instance of BaseModel, save to JSON file'
+        if 'BaseModel' in arg:
+            new = models.BaseModel()
+            print('{}'.format(new.id))
+
+    def do_show(self, arg):
+        'Prints the string representation of an instance \
+        based on the class name and id'
+        if (len(arg.split()) == 0):
+            print('** class name is missing **')
+        elif 'BaseModel' not in arg:
+            print('** class doesn''t exist **')
+        elif (len(arg.split()) < 2):
+            print('** instance id missing **')
+        elif arg.split()[1] != self.obj.__class__.__name__: #not working correctly
+            print('** no instance found **')
+        else:
+            print(self.obj) #not sure if this is working
 
 if __name__ == '__main__':
     hbnb().cmdloop()
