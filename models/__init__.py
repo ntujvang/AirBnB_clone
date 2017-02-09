312f0f25ccd0e79d7e0b3c8dc5c8a3814328a88f
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-09 22:20:02 +0000

    add import user

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from models.engine import file_storage
 from models.base_model import BaseModel
+from models.user import User
 
 storage = file_storage.FileStorage()
 storage.reload()
