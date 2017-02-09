0e86fa3dfbc65bd289f5ef861915187f44eee6c9
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-09 22:43:08 +0000

    add import Amenity

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
+from models.amenity import Amenity
 
 storage = file_storage.FileStorage()
 storage.reload()
