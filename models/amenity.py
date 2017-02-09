fcf30cd5937a773ac808880ed034dd58e8ecc9f8
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-09 22:40:56 +0000

    create amenity class

---
+++ models/amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+This is the 'amenity' module.
+
+amenity contains the class 'Amenity', a sub-class of BaseModel.
+'''
+
+
+class Amenity(BaseModel):
+    '''This is the 'Amenity' class.
+
+    The class contains one public attribute, 'name'.
+    '''
+    def __init__(self):
+        '''This is the initialization method.
+        '''
+        self.name = ''
