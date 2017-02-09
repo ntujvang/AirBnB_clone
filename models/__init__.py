01e72c0e37fcadab6b60576b7a213a1f02dd9620
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-02-09 23:00:04 +0000

    add import Review

--- models/__init__.py
+++ models/__init__.py
@@ -6,6 +6,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
+from models.review import Review
 
 storage = file_storage.FileStorage()
 storage.reload()
