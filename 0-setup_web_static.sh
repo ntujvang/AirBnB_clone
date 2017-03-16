fead7958373884a44d5526bdfec351de15f8425b
ntujvang <96@holbertonschool.com>
2017-03-16 00:52:48 +0000

    removed some ~

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
-echo -e '<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' > /data/web_static/releases/test/index.html
+echo -e '<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\n\tHolberton School\n</body>\n</html>' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu ~/data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "29i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
