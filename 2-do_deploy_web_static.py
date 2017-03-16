3a2e8d339102fb7b08c2799e1299d93154542cd3
ntujvang <96@holbertonschool.com>
2017-03-16 06:44:24 +0000

    changed cp/rm into mv

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,19 +4,6 @@ from fabric.api import *
 env.hosts = ['54.89.103.152', '54.91.31.60']
 
 
-def do_pack():
-    """
-    Creates a .tgz archive of all contents from web_static
-    """
-    now = time.strftime('%Y%m%d%H%M%S')
-    try:
-        local("sudo mkdir -p versions")
-        local("sudo tar -cvzf versions/web_static_"+now+".tgz web_static/")
-        print("versions/web_static_"+now+".tgz")
-    except:
-        return None
-
-
 def do_deploy(archive_path):
     """
     This function deploys our archive onto our webservers!
@@ -26,12 +13,11 @@ def do_deploy(archive_path):
         notgz_tag = archive_path[9:-4]
         new_dir = ("/data/web_static/releases/" + notgz_tag)
         run('sudo mkdir -p /tmp')
-        put(archive_path, '/tmp/')
-        run('sudo mkdir -p {}/'.format(new_dir))
+        put(archive_path, '/tmp')
+        run('sudo mkdir -p {}'.format(new_dir))
         run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, new_dir))
         run('sudo rm /tmp/{}'.format(file_name))
-        run('sudo cp -R {}/web_static/* {}'.format(new_dir, new_dir))
-        run('sudo rm -rf {}/web_static/*'.format(new_dir))
+        run("sudo mv -f {}/web_static/* {}".format(new_dir, new_dir))
         run('sudo rm -rf {}/web_static'.format(new_dir))
         run('sudo rm -rf /data/web_static/current')
         run('sudo ln -s {}/ /data/web_static/current'.format(new_dir))
