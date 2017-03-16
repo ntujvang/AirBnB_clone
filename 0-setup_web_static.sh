461d7f0557719543e381fa4fb4e624d183c4181c
ntujvang <96@holbertonschool.com>
2017-03-16 00:50:33 +0000

    shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
-sudo echo -e '<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' > /data/web_static/releases/test/index.html
+echo -e '<!DOCTYPE html>\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' > /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu ~/data/
 sudo sed -i "29i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-enabled/default
